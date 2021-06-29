ae22384030c6aba418fa891acadc8c8404f496da
WangZhuo <zhuowangy2k@outlook.com>
2021-06-29 21:21:56 +0800

    [update] use honey badger for main function

--- main/localstart.go
+++ main/localstart.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"acc/Benchmark"
+	"acc/benchmark"
 	"acc/config"
+	"acc/consensus"
 	"acc/logger"
 	"acc/pb"
-	"acc/rbc"
 	"crypto/rand"
 )
 
@@ -15,30 +15,29 @@ func f(s config.Start) {
 	//run a consensus protocol, here is a send protocol for example
 	// generate Data random
 	conf := s.GetConfig()
-	if conf.MyID == 1 {
-		TX := 250
-		num := conf.Txnum
-		data := make([]byte, num*TX)
-		n, err := rand.Read(data)
-		if err != nil {
-			log.Fatalf("random read data fail: %s", err.Error())
-		}
-		if n != num*TX {
-			log.Fatal("fail to read enough bytes")
-		}
-		log.Infof("input message's length is %d", len(data))
-		Benchmark.Begin("testRBC", conf.MyID)
-		go rbc.RBCBroadcast(&pb.Message{
-			Id:       "testRBC",
-			Sender:   1,
-			Receiver: 0,
-			Data:     data,
-		}, s)
-	}
-
-	_, err := rbc.RBCDeliver("testRBC", s)
+	benchmark.Create("HB")
+	TX := 250
+	num := conf.Txnum
+	data := make([]byte, num*TX)
+	n, err := rand.Read(data)
 	if err != nil {
-		log.Error("RBC deliver test fail: %s", err.Error())
+		log.Fatalf("random read data fail: %s", err.Error())
 	}
-	Benchmark.End("testRBC", conf.MyID)
+	if n != num*TX {
+		log.Fatal("fail to read enough bytes")
+	}
+	log.Infof("input message's length is %d", len(data))
+	benchmark.Begin("HB", conf.MyID)
+
+	results, err := consensus.Consensus(&pb.Message{
+		Id:       "HoneyBadger",
+		Sender:   1,
+		Receiver: 0,
+		Data:     data,
+	}, s)
+
+	consenedTXnum := len(results) * len(results[0].Data) / TX
+	log.Infof("results length: %d, tx nums: %d", len(results), consenedTXnum)
+	benchmark.End("HB", conf.MyID)
+	benchmark.Nums("HB", conf.MyID, consenedTXnum)
 }
