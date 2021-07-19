61d019ac36649503fa3f6a4216679934349ad6ce
WangZhuo <zhuowangy2k@outlook.com>
2021-07-19 15:57:49 +0800

    [update] add remote/local flag to main

--- main/main.go
+++ main/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"acc/benchmark"
 	"acc/config"
+	"acc/logger"
+	"os"
 )
 
+const usage = "./start local|remoteGen|remote"
+
 func main() {
-	// command process or something
-	start := config.NewLocalStart(f, "./config.yaml")
-	start.Run()
-	_ = benchmark.BenchmarkOuput()
+	lo := logger.NewLogger("main")
+	if len(os.Args) != 2 {
+		lo.Fatal(usage)
+	}
+	if os.Args[1] == "local" {
+		start := config.NewLocalStart(f, "./config.yaml")
+		start.Run()
+		_ = benchmark.BenchmarkOuput()
+	} else if os.Args[1] == "remoteGen" {
+		log.Fatal("not be implemented yet")
+	} else if os.Args[1] == "remote" {
+		log.Fatal("not be implemented yet")
+	} else {
+		lo.Fatal(usage)
+	}
 }
