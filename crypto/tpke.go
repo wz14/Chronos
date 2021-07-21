b58d0a4e68d824e75537fa1305a611a734f9228d
WangZhuo <zhuowangy2k@outlook.com>
2021-07-21 16:42:46 +0800

    [update] add marshal && unmarshal to tpke

--- crypto/tpke.go
+++ crypto/tpke.go
@@ -12,7 +12,9 @@ package crypto
 //		   this project package name
 
 import (
+	"encoding/base64"
 	tpk "github.com/WangZhuo2000/tpke"
+	"github.com/pkg/errors"
 	"strconv"
 )
 
@@ -88,8 +90,6 @@ func (t *DefaultTpke) Decrypt(decShares map[string]DecryptionShare, ctBytes []by
 }
 
 type TPKE struct {
-	sks  *tpk.SecretKeySet
-	pks  *tpk.PublicKeySet
 	id   int
 	tpke *DefaultTpke
 }
@@ -106,8 +106,6 @@ func NewTPKE(N int, t int) []*TPKE {
 		tpke, _ := NewDefaultTpke(N, secretKeySet.KeyShareUsingString(strconv.Itoa(i)).Serialize(),
 			publicKeySet.Serialize())
 		tpkes = append(tpkes, &TPKE{
-			sks:  secretKeySet,
-			pks:  publicKeySet,
 			id:   i,
 			tpke: tpke,
 		})
@@ -134,4 +132,84 @@ func (t *TPKE) Dec(m map[int][]byte, ct []byte) []byte {
 	}
 	msg, _ := t.tpke.Decrypt(mm, ct)
 	return msg
+}
+
+/*
+	marshal && unmarshal
+
+	1.int(id) -> string
+	X.tpke ->
+		2.threshold    int
+		3.publicKey    *tpk.PublicKey
+		4.publicKeySet *tpk.PublicKeySet
+		5.secretKey    *tpk.SecretKeyShare
+		(no need) decShares    map[string]*tpk.DecryptionShare
+*/
+
+func (t *TPKE) Marshal() ([]string, error) {
+	result := make([]string, 5)
+	// marshal id
+	result[0] = strconv.Itoa(t.id)
+	// marshal tpke.threshold
+	result[1] = strconv.Itoa(t.tpke.threshold)
+	// marshal tpke.publickey
+	byts := t.tpke.publicKey.Serialize()
+	b := base64.StdEncoding.EncodeToString(byts[:])
+	result[2] = b
+	// marshal tpke.publickeySet
+	pksbyts := t.tpke.publicKeySet.Serialize()
+	b2 := base64.StdEncoding.EncodeToString(pksbyts)
+	result[3] = b2
+	// marshal tpke.secretKey
+	skbyts := t.tpke.secretKey.Serialize()
+	b3 := base64.StdEncoding.EncodeToString(skbyts[:])
+	result[4] = b3
+	return result, nil
+}
+
+func (t *TPKE) UnMarshal(s []string) error {
+	// unmarshal 1
+	atoi, err := strconv.Atoi(s[0])
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal id fail")
+	}
+	t.id = atoi
+	// unmarshal 2
+	thres, err := strconv.Atoi(s[1])
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal tpke.threshold fail")
+	}
+	// unmarshal 3
+	pkbyts, err := base64.StdEncoding.DecodeString(s[2])
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal tpke.publickey fail")
+	}
+	pkfixbyts := [96]byte{}
+	copy(pkfixbyts[:], pkbyts)
+	pk := tpk.NewPublicKeyFromBytes(pkfixbyts)
+	// unmarshal 4
+	pksbyts, err := base64.StdEncoding.DecodeString(s[3])
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal tpke.publickeyset fail")
+	}
+	pks, err := tpk.NewPublicKeySetFromBytes(pksbyts)
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal tpke.publickeyset fail")
+	}
+	// unmarshal 5
+	skbyts, err := base64.StdEncoding.DecodeString(s[4])
+	if err != nil {
+		return errors.Wrapf(err, "unmarshal tpke.publickeyset fail")
+	}
+	skfixbyts := [32]byte{}
+	copy(skfixbyts[:], skbyts)
+	sk := tpk.NewSecretKeyShareFromBytes(skfixbyts)
+	t.tpke = &DefaultTpke{
+		threshold:    thres,
+		publicKey:    pk,
+		publicKeySet: pks,
+		secretKey:    sk,
+		decShares:    make(map[string]*tpk.DecryptionShare),
+	}
+	return nil
 }
--- crypto/tpke_test.go
+++ crypto/tpke_test.go
@@ -55,3 +55,56 @@ func TestNewTPKE4(t *testing.T) {
 		t.Errorf("decrypt error: %s", string(msg))
 	}
 }
+
+func TestTPKE_Marshal(t *testing.T) {
+	tpkes := NewTPKE(4, 1)
+	m, err := tpkes[1].Marshal()
+	if err != nil {
+		t.Errorf("marshal fail %s", err.Error())
+	}
+	newtpke := TPKE{}
+	err = newtpke.UnMarshal(m)
+	if err != nil {
+		t.Errorf("unmarshal fail %s", err.Error())
+	}
+	message := []byte("0xdeadbeef")
+	ct := tpkes[3].Enc(message)
+	s0 := tpkes[0].DecShare(ct)
+	s1 := newtpke.DecShare(ct)
+	s := map[int][]byte{0: s0, 1: s1}
+	msg := tpkes[2].Dec(s, ct)
+	if !bytes.Equal(msg, message) {
+		t.Errorf("decrypt error %s", message)
+	}
+}
+
+func TestTPKE_UnMarshal(t *testing.T) {
+	tpkes := NewTPKE(4, 1)
+	m, err := tpkes[0].Marshal()
+	if err != nil {
+		t.Errorf("marshal fail %s", err.Error())
+	}
+	new0tpke := TPKE{}
+	err = new0tpke.UnMarshal(m)
+	if err != nil {
+		t.Errorf("unmarshal fail %s", err.Error())
+	}
+	m, err = tpkes[1].Marshal()
+	if err != nil {
+		t.Errorf("marshal fail %s", err.Error())
+	}
+	new1tpke := TPKE{}
+	err = new1tpke.UnMarshal(m)
+	if err != nil {
+		t.Errorf("unmarshal fail %s", err.Error())
+	}
+	message := []byte("0xdeadbeef")
+	ct := tpkes[3].Enc(message)
+	s0 := new0tpke.DecShare(ct)
+	s1 := new1tpke.DecShare(ct)
+	s := map[int][]byte{0: s0, 1: s1}
+	msg := tpkes[2].Dec(s, ct)
+	if !bytes.Equal(msg, message) {
+		t.Errorf("decrypt error %s", message)
+	}
+}
